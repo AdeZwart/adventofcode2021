1e290b7307e1399c312ad0e764fe9914b7c27f67
André de Zwart <andredezwart@gmail.com>
2021-12-05 15:57:25 +0100

    Add Day06 overrides

--- AdventOfCode2021/Day06.cs
+++ AdventOfCode2021/Day06.cs
@@ -4,5 +4,14 @@ namespace AzW.AdventOfCode2021.Year2021
 {
     class Day06 : Day
     {
+        public override object ExecutePart1()
+        {
+            return base.ExecutePart1();
+        }
+
+        public override object ExecutePart2()
+        {
+            return base.ExecutePart2();
+        }
     }
 }
